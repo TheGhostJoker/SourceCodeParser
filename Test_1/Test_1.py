b98b9cc0e4e4d0fd9b611c3a4feb37c172deff9d
Rishit Chaudhary <rishit.c@live.com>
2019-06-10 13:40:01 +0530

    The code parser
    This will parse the source code and break it into required blocks based on the tags.

--- Test_1/Test_1.py
+++ Test_1/Test_1.py
@@ -1,1 +1,39 @@
-print("Hello")
+import os
+
+file = open('test.txt', 'r')
+code_blocks = []
+file1 = None
+
+for line in file:
+
+    line = line.lower()
+    line = line.strip()
+    print("> " + line)
+
+    # keep writing data into the file until you reach the next tag
+    if line.startswith("//cpc_tag:"):
+        # when you reach a new tag then close the current file tag, and open a new one
+        if file1 is not None:
+            file1.close()
+        file1 = open(line[10:] + '.txt', 'w')
+
+        # adding a record to the list of blocks
+        code_blocks.append(line[10:])
+
+    else:
+        if (file1 is not None) and line:
+            file1.write(line + '\n')
+
+file1 = open('blocks.py', 'w')
+file1.write("code_blocks = [")
+
+lim = len(code_blocks)
+ctr = 0
+
+for ele in code_blocks:
+    if ctr < lim - 1:
+        file1.write("'" + ele + "'" + ',')
+    else:
+        file1.write("'" + ele + "'" + ']')
+    ctr += 1
+file1.close()
